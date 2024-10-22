7a5a70bbfa28078012f5a6c0e4712aa602df8a29
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-22 16:35:55 +0200

    ADD: riferimento alla seconda immagine

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ console.log('Primi passi col DOM')
 
 // vado ad accendere la lampadina
 
+// mi recupero il riferimento all bottone accedi
 
 const light = document.getElementById('accendi')
 
-light.addEventListener('click', function(){
+// mi recupero il riferimento all'immagine
+
+const image = document.getElementById('img')
+
+// vado a eseguire lo spegnimento della lampadina
+
+// eseguo la funzione
+
+light.addEventListener('click', function () {
     console.log('hai acceso la lampadina')
-    let img = document.createElement("img");
-    img.src = "img/yellow_lamp.png";
-    }
-)
+    image.src = "img/yellow_lamp.png";
+})
+
+
+light.addEventListener('click',function(){
+    console.log('hai spento la lampadina')
+})
+
 
 
 
