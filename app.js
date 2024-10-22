3a86b8cc1c3fb1d5d7634bf99ff22fbeae67be0d
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-22 15:00:09 +0200

    ADD: imposto la funzione

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log('Primi passi col DOM')
+
+
+
+// vado ad accendere la lampadina
+
+
+const light = document.getElementById('accendi')
+
+myButton.addEventListener('click',function(){
+    console.log('ho cliccato')
+    light element.addEventListener()
+    }
+)
+
+
+//myButton.addEventListener
+
+
+
